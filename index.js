32f8cd6b3ab77a028464615b11db9b1321edbce8
Mohamad M <54006217+thedev132@users.noreply.github.com>
2023-01-14 17:55:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
+const fetch = require("node-fetch");
 require("dotenv").config(); //to start process from .env file
 const {Discord, EmbedBuilder, Client, Intents, GatewayIntentBits}=require("discord.js");
 const schedule = require('node-schedule');
-const client=new Client({
-    intents:[
-        GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-    ]
-});
+const client = new Client({ intents: [ 
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,] });
+const prefix = "!"
 client.once("ready", () =>{
     console.log("BOT IS ONLINE"); //message when bot is online
+})
+
+async function getPlayers() {
+    const response = await fetch("https://api.minetools.eu/ping/cmsband.cluster.ws")
+    const data = await response.json();
+    const sample = data.players.sample
+    var playerNames = []
+    sample.forEach(element => {
+        playerNames.push(element.name)
+    }); 
+    return playerNames
+}
+
+client.on("messageCreate", message => {
+    if(message.content.startsWith(`${prefix}players`)){
+        getPlayers().then(function(response){
+            var playerNames = "Players online are: \n"
+            response.forEach(function(element) {
+                playerNames += `**${element}**\n`
+            })
+
+            
+            if (response.length == 0) {
+                var messageEmbed = new EmbedBuilder()
+                .setColor('#ED4245')
+                .setTitle(`No Online Players`)
+                .setDescription(`There are no online players at the moment!`)
+                message.channel.send({embeds: [messageEmbed]})
+            }
+            else if (response.length > 0) {
+                var messageEmbed = new EmbedBuilder()
+                .setColor('#57F287')
+                .setTitle(`Online Players`)
+                .setDescription(`${playerNames}`)
+                message.channel.send({embeds: [messageEmbed]})
+            }
+
+        })
+
+        
+
+    } else if (message.content.startsWith(prefix + "avatar")) {
+        message.reply(message.author.avatarURL);
+
+    }
 })
 
 schedule.scheduleJob('0 17 * * 5', () => { 
